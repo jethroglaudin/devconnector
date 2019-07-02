2eb77b5e5c74de4532b7c7ae3fdc4dd27a5b948c
Jethro Glaudin <jethro_glaudin@my.uri.edu>
2019-07-01 23:28:47 -0400

    imported Footer, Landing, and Navbar

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,22 @@
-import React from 'react';
-import './App.css';
+import React, { Component } from 'react';
+import Navbar from './component/layout/Navbar';
+import Footer from './component/layout/Footer';
+import Landing from './component/layout/Landing';
 
-function App() {
-  return (
-    <div className="App">
-     <h1>My React App</h1>
-    </div>
-  );
+
+ class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <Navbar />
+        <Landing />
+        <Footer />
+        
+        
+      </div>
+    )
+  }
 }
+
 
 export default App;
