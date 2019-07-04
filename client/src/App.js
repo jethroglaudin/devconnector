5c7f0c18b4c5e7bc2333a0d767f06dfd914f810a
Jethro Glaudin <jethro_glaudin@my.uri.edu>
2019-07-03 22:27:54 -0400

    imported Register.js and Login.js and placed them within div container

--- client/src/App.js
+++ client/src/App.js
@@ -1,24 +1,30 @@
-import React, { Component } from 'react';
-import Navbar from './component/layout/Navbar';
-import Footer from './component/layout/Footer';
-import Landing from './component/layout/Landing';
+import React, { Component } from "react";
+import { BrowserRouter as Router, Route } from "react-router-dom";
 
-import './App.css';
+import Navbar from "./component/layout/Navbar";
+import Footer from "./component/layout/Footer";
+import Landing from "./component/layout/Landing";
+import Register from "./component/auth/Register";
+import Login from "./component/auth/Login";
 
+import "./App.css";
 
- class App extends Component {
+class App extends Component {
   render() {
     return (
-      <div className="App">
-        <Navbar />
-        <Landing />
-        <Footer />
-        
-        
-      </div>
-    )
+      <Router>
+        <div className="App">
+          <Navbar />
+          <Route exact path='/' component={Landing} />
+          <div className="container">
+            <Route exact path="/register" component={Register} />
+            <Route exact path="/login" component={Login} />
+          </div>
+          <Footer />
+        </div>
+      </Router>
+    );
   }
 }
-
 
 export default App;
