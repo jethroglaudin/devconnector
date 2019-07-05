c808a61c8dadc998b1ac520d419097ecba0ddeb8
Jethro Glaudin <jethro_glaudin@my.uri.edu>
2019-07-05 15:17:42 -0400

    updated App.js to cehck for jwtToken to keep user logged in should jwt token be in local storage

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,10 @@
-import React, { Component } from 'react';
-import { BrowserRouter as Router, Route } from 'react-router-dom';
-import { Provider } from 'react-redux';
-import store from './store';
-
+import React, { Component } from "react";
+import { BrowserRouter as Router, Route } from "react-router-dom";
+import jwt_decode from "jwt-decode";
+import setAuthToken from "./utils/setAuthToken";
+import { setCurrentUser } from "./actions/authActions";
+import { Provider } from "react-redux";
+import store from "./store";
 
 import Navbar from "./component/layout/Navbar";
 import Footer from "./component/layout/Footer";
@@ -12,23 +14,31 @@ import Login from "./component/auth/Login";
 
 import "./App.css";
 
-
+// Check for token
+if (localStorage.jwtToken) {
+  // Set auth token header auth
+  setAuthToken(localStorage.jwt);
+  // Decode token and get user info and expiration
+  const decoded = jwt_decode(localStorage.jwtToken);
+  // Set user and isAuthenticated
+  store.dispatch(setCurrentUser(decoded));
+}
 
 class App extends Component {
   render() {
     return (
-      <Provider store = { store }>
-      <Router>
-        <div className="App">
-          <Navbar />
-          <Route exact path='/' component={Landing} />
-          <div className="container">
-            <Route exact path="/register" component={Register} />
-            <Route exact path="/login" component={Login} />
+      <Provider store={store}>
+        <Router>
+          <div className="App">
+            <Navbar />
+            <Route exact path="/" component={Landing} />
+            <div className="container">
+              <Route exact path="/register" component={Register} />
+              <Route exact path="/login" component={Login} />
+            </div>
+            <Footer />
           </div>
-          <Footer />
-        </div>
-      </Router>
+        </Router>
       </Provider>
     );
   }
