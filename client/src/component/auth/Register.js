2a0a3ab852157d665682f54859ff6f3456d30dc8
Jethro Glaudin <jethro_glaudin@my.uri.edu>
2019-07-06 21:16:38 -0400

    added componentDidMount lifecycle method so that if user's auth isAuthenticated to send them to the dashboard page

--- client/src/component/auth/Register.js
+++ client/src/component/auth/Register.js
@@ -19,6 +19,14 @@ class Register extends Component {
     this.onChange = this.onChange.bind(this);
     this.onSubmit = this.onSubmit.bind(this);
   }
+
+  componentDidMount(){
+    if(this.props.auth.isAuthenticated){
+      this.props.history.push('/dashboard');
+    }
+  }
+
+
   // life-cycle method will run when component receive new props. 
 
   componentWillReceiveProps(nextProps){
