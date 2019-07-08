c05ecb6bd0dd4cf4962af288675c8d808e4cd7c1
Jethro Glaudin <jethro_glaudin@my.uri.edu>
2019-07-07 22:23:15 -0400

    imported Dashboard component and added new store.disptach func to clear the current profile

--- client/src/App.js
+++ client/src/App.js
@@ -3,6 +3,8 @@ import { BrowserRouter as Router, Route } from "react-router-dom";
 import jwt_decode from "jwt-decode";
 import setAuthToken from "./utils/setAuthToken";
 import { setCurrentUser, logoutUser } from "./actions/authActions";
+import { clearCurrentProfile } from "./actions/profileActions";
+
 import { Provider } from "react-redux";
 import store from "./store";
 
@@ -11,6 +13,7 @@ import Footer from "./component/layout/Footer";
 import Landing from "./component/layout/Landing";
 import Register from "./component/auth/Register";
 import Login from "./component/auth/Login";
+import Dashboard from "./component/dashboard/Dashboard";
 
 import "./App.css";
 
@@ -29,6 +32,7 @@ if (localStorage.jwtToken) {
     // Logout user
     store.dispatch(logoutUser())
     // TODO: Clear current profile
+    store.dispatch(clearCurrentProfile())
     // Redirect to login
     window.location.href = '/login';
 
@@ -46,6 +50,7 @@ class App extends Component {
             <div className="container">
               <Route exact path="/register" component={Register} />
               <Route exact path="/login" component={Login} />
+              <Route exact path="/dashboard" component={Dashboard} />          
             </div>
             <Footer />
           </div>
