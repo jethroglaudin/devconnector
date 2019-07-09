9358367cd3709ef0fb3b152e399a1b45f1c4de44
Jethro Glaudin <jethro_glaudin@my.uri.edu>
2019-07-09 18:47:08 -0400

    added deleteAccount action that makes an axios delete request deleting the user's profile and auth credentials

--- client/src/actions/profileActions.js
+++ client/src/actions/profileActions.js
@@ -4,6 +4,8 @@ import {
   GET_PROFILE,
   PROFILE_LOADING,
   CLEAR_CURRENT_PROFILE,
+  GET_ERRORS,
+  SET_CURRENT_USER
 } from './types';
 
 // Get current profile
@@ -24,6 +26,37 @@ export const getCurrentProfile = () => dispatch => {
       })
     );
 };
+
+// Create Profile
+export const createProfile = (profileData, history) => dispatch => {
+  axios
+    .post('/api/profile', profileData)
+    .then(res => history.push('/dashboard'))
+    .catch(err => 
+      dispatch({
+      type: GET_ERRORS,
+      payload: err.response.data
+
+    }))
+}
+
+// Delete account & profile
+export const deleteAccount = () => dispatch => {
+  if(window.confirm('Are you sure? This can NOT be undone!')) {
+    axios
+      .delete('/api/profile')
+      .then(res=> 
+        dispatch({
+          type: SET_CURRENT_USER,
+          payload: {}
+        })
+      ).catch(err =>
+        dispatch({
+          type: GET_ERRORS,
+          payload: err.response.data
+        }))
+  }
+}
 
 
 
