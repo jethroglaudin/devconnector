2a2a76c0bec7fe71454193ef56ddd0dc864d4b3e
Jethro Glaudin <jethro_glaudin@my.uri.edu>
2019-07-08 22:51:41 -0400

    Dashboard.js updated

--- client/src/components/dashboard/Dashboard.js
+++ client/src/components/dashboard/Dashboard.js
@@ -26,9 +26,9 @@ class Dashboard extends Component {
             // User is logged in but has no profile
             dashboardContent = (
                 <div>
-                    <p className="lead text-muted">Welcome {user.name}</p>
+                    <p className="lead text-muted">Welcome { user.name }</p>
                     <p>You have not yet setup a profile, please add some info</p>
-                    <Link to='/create-profile' classname="btn btn-lg btn-info">
+                    <Link to='/create-profile' className="btn btn-lg btn-info">
                     Create Profile
                     </Link>
                 </div>
