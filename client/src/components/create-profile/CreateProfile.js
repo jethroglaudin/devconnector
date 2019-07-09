e9b6766f59589d803a01f30000f8305e20ebc663
Jethro Glaudin <jethro_glaudin@my.uri.edu>
2019-07-09 00:12:54 -0400

    imported all the sperate form groups. Imported connect from react-redux to connect mapStateToProps to CreateProfile

--- client/src/components/create-profile/CreateProfile.js
+++ client/src/components/create-profile/CreateProfile.js
@@ -1,7 +1,11 @@
 import React, { Component } from "react";
 import { connect } from "react-redux";
 import PropTypes from "prop-types";
-import TextFeildGroup from "../common/TextFieldGroup";
+import TextFieldGroup from "../common/TextFieldGroup";
+import TextAreaFieldGroup from "../common/TextAreaFieldGroup";
+import InputGroup from "../common/InputGroup";
+import SelectListGroup from "../common/SelectListGroup";
+
 
 class CreateProfile extends Component {
   constructor(props) {
@@ -53,4 +57,4 @@ const mapStateToProps = state => ({
   errors: state.errors
 });
 
-export default connect(null)(CreateProfile);
+export default connect(mapStateToProps)(CreateProfile);
