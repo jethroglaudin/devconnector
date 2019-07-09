9ba6167f7dc0623c7f82c069ce11e58b380fd844
Jethro Glaudin <jethro_glaudin@my.uri.edu>
2019-07-08 22:50:24 -0400

    added Dashboard component

--- client/src/App.js
+++ client/src/App.js
@@ -1,41 +1,42 @@
-import React, { Component } from "react";
-import { BrowserRouter as Router, Route } from "react-router-dom";
-import jwt_decode from "jwt-decode";
-import setAuthToken from "./utils/setAuthToken";
-import { setCurrentUser, logoutUser } from "./actions/authActions";
-import { clearCurrentProfile } from "./actions/profileActions";
+import React, { Component } from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import jwt_decode from 'jwt-decode';
+import setAuthToken from './utils/setAuthToken';
+import { setCurrentUser, logoutUser } from './actions/authActions';
+import { clearCurrentProfile } from './actions/profileActions';
 
-import { Provider } from "react-redux";
-import store from "./store";
+import { Provider } from 'react-redux';
+import store from './store';
 
-import Navbar from "./component/layout/Navbar";
-import Footer from "./component/layout/Footer";
-import Landing from "./component/layout/Landing";
-import Register from "./component/auth/Register";
-import Login from "./component/auth/Login";
-import Dashboard from "./component/dashboard/Dashboard";
+import Navbar from './components/layout/Navbar';
+import Footer from './components/layout/Footer';
+import Landing from './components/layout/Landing';
+import Register from './components/auth/Register';
+import Login from './components/auth/Login';
+import Dashboard from './components/dashboard/Dashboard';
 
-import "./App.css";
+
+import './App.css';
+
 
 // Check for token
 if (localStorage.jwtToken) {
   // Set auth token header auth
-  setAuthToken(localStorage.jwt);
-  // Decode token and get user info and expiration
+  setAuthToken(localStorage.jwtToken);
+  // Decode token and get user info and exp
   const decoded = jwt_decode(localStorage.jwtToken);
   // Set user and isAuthenticated
   store.dispatch(setCurrentUser(decoded));
 
   // Check for expired token
   const currentTime = Date.now() / 1000;
-  if(decoded.exp < currentTime) {
+  if (decoded.exp < currentTime) {
     // Logout user
-    store.dispatch(logoutUser())
-    // TODO: Clear current profile
-    store.dispatch(clearCurrentProfile())
+    store.dispatch(logoutUser());
+    // Clear current Profile
+    store.dispatch(clearCurrentProfile());
     // Redirect to login
     window.location.href = '/login';
-
   }
 }
 
