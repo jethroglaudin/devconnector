c37c765574bc2b298cbbc40d502c48e0be818115
Jethro Glaudin <jethro_glaudin@my.uri.edu>
2019-07-09 20:16:07 -0400

    imported EditProfile component

--- client/src/App.js
+++ client/src/App.js
@@ -17,7 +17,7 @@ import Register from "./components/auth/Register";
 import Login from "./components/auth/Login";
 import Dashboard from "./components/dashboard/Dashboard";
 import CreateProfile from "./components/create-profile/CreateProfile";
-
+import EditProfile from "./components/edit-profile/EditProfile";
 
 import "./App.css";
 
@@ -57,9 +57,19 @@ class App extends Component {
                 <PrivateRoute exact path="/dashboard" component={Dashboard} />
               </Switch>
               <Switch>
-                <PrivateRoute exact path="/create-profile" component={CreateProfile} />
+                <PrivateRoute
+                  exact
+                  path="/create-profile"
+                  component={CreateProfile}
+                />
               </Switch>
-              
+              <Switch>
+                <PrivateRoute
+                  exact
+                  path="/edit-profile"
+                  component={EditProfile}
+                />
+              </Switch>
             </div>
             <Footer />
           </div>
