3e5223d3d8de873d1d65c746052e544ceba46a5e
Jethro Glaudin <jethro_glaudin@my.uri.edu>
2019-07-10 13:48:58 -0400

    created addExperience action

--- client/src/actions/profileActions.js
+++ client/src/actions/profileActions.js
@@ -1,4 +1,4 @@
-import axios from 'axios';
+import axios from "axios";
 
 import {
   GET_PROFILE,
@@ -6,13 +6,13 @@ import {
   CLEAR_CURRENT_PROFILE,
   GET_ERRORS,
   SET_CURRENT_USER
-} from './types';
+} from "./types";
 
 // Get current profile
 export const getCurrentProfile = () => dispatch => {
   dispatch(setProfileLoading());
   axios
-    .get('/api/profile')
+    .get("/api/profile")
     .then(res =>
       dispatch({
         type: GET_PROFILE,
@@ -30,35 +30,48 @@ export const getCurrentProfile = () => dispatch => {
 // Create Profile
 export const createProfile = (profileData, history) => dispatch => {
   axios
-    .post('/api/profile', profileData)
-    .then(res => history.push('/dashboard'))
-    .catch(err => 
+    .post("/api/profile", profileData)
+    .then(res => history.push("/dashboard"))
+    .catch(err =>
       dispatch({
-      type: GET_ERRORS,
-      payload: err.response.data
+        type: GET_ERRORS,
+        payload: err.response.data
+      })
+    );
+};
 
-    }))
-}
+//Add experience
+export const addExperience = (expData, history) => dispatch => {
+  axios
+    .post("/api/profile/experience/", expData)
+    .then(res => history.push("/dashboard"))
+    .catch(err =>
+      dispatch({
+        type: GET_ERRORS,
+        payload: err.response.data
+      })
+    );
+};
 
 // Delete account & profile
 export const deleteAccount = () => dispatch => {
-  if(window.confirm('Are you sure? This can NOT be undone!')) {
+  if (window.confirm("Are you sure? This can NOT be undone!")) {
     axios
-      .delete('/api/profile')
-      .then(res=> 
+      .delete("/api/profile")
+      .then(res =>
         dispatch({
           type: SET_CURRENT_USER,
           payload: {}
         })
-      ).catch(err =>
+      )
+      .catch(err =>
         dispatch({
           type: GET_ERRORS,
           payload: err.response.data
-        }))
+        })
+      );
   }
-}
-
-
+};
 
 // Profile loading
 export const setProfileLoading = () => {
