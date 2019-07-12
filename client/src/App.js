3d8f1a0e09196836633647ecf466dee261c6238c
Jethro Glaudin <jethro_glaudin@my.uri.edu>
2019-07-12 11:59:35 -0400

    imported Post and assigned it as a PrivateRoute

--- client/src/App.js
+++ client/src/App.js
@@ -24,6 +24,8 @@ import Profiles from "./components/profiles/Profiles";
 import Profile from "./components/profile/Profile";
 import NotFound from "./components/not-found/NotFound";
 import Posts from "./components/posts/Posts";
+import Post from "./components/post/Post";
+
 
 
 
@@ -100,6 +102,13 @@ class App extends Component {
                   exact
                   path="/feed"
                   component={Posts}
+                />
+              </Switch>
+              <Switch>
+                <PrivateRoute
+                  exact
+                  path="/post/:id"
+                  component={Post}
                 />
               </Switch>
               <Route exact path="/not-found" component={NotFound} />
