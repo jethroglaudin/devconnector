2a9a31eff9537abdc3575aba63442b5900643ad4
Jethro Glaudin <jethro_glaudin@my.uri.edu>
2019-07-12 15:44:10 -0400

    fixed another typo within server.js

--- server.js
+++ server.js
@@ -41,9 +41,10 @@ if (process.env.NODE_ENV === "production") {
   app.use(express.static("client/build"));
 
   app.get("*", (req, res) => {
-    res.sendFile(path.resolve(__dirname, "client", "buiild", "index.html"));
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
   });
 }
+
 
 const port = process.env.PORT || 5000;
 
