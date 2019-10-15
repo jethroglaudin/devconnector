adf06874a59799b561fa8a9b41bc00f985ee27ca
Jethro Glaudin <jethro_glaudin@my.uri.edu>
2019-10-15 15:49:20 -0400

    changed App name from DevConnector to CONLIGO

--- client/src/components/auth/Register.js
+++ client/src/components/auth/Register.js
@@ -63,7 +63,7 @@ class Register extends Component {
             <div className="col-md-8 m-auto">
               <h1 className="display-4 text-center">Sign Up</h1>
               <p className="lead text-center">
-                Create your DevConnector account
+                Create your CONLIGO account
               </p>
               <form noValidate onSubmit={this.onSubmit}>
               <TextFieldGroup 
