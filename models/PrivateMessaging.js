58d46694472f00421a40820051640b39b04c6587
Jethro Glaudin <jethro_glaudin@my.uri.edu>
2019-10-16 22:56:38 -0400

    created PrivateMessaging Schema

---
+++ models/PrivateMessaging.js
@@ -0,0 +1,49 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const PrivateMessagingSchema = newSchema({
+  user: {
+    type: Schema.Types.ObjectId,
+    ref: "users"
+  },
+  text: {
+    type: String,
+    required: true
+  },
+  name: {
+    type: String
+  },
+
+  avatar: {
+    type: String
+  },
+
+  replies: [
+    {
+      user: {
+        type: Schema.Types.ObjectId,
+        ref: "users"
+      },
+
+      text: {
+          type: String, 
+          required: true
+      },
+
+      name: {
+          type: String
+      },
+
+      avatar: {
+          type: String
+      }
+    }
+  ],
+  date: {
+      type: Date,
+      default: Date.now
+  }
+});
+
+
+module.exports = PrivateMessaging = mongoose.model("privateMessages", PrivateMessagingSchema)
