1e771a3a8003b14734877922ad2d8e751be849db
Jethro Glaudin <jethro_glaudin@my.uri.edu>
2019-10-23 15:48:02 -0400

    added 6 new REDUX action types

--- client/src/actions/types.js
+++ client/src/actions/types.js
@@ -11,6 +11,12 @@ export const GET_POSTS = 'GET_POSTS';
 export const GET_POST = 'GET_POST';
 export const ADD_POST = 'ADD_POST';
 export const DELETE_POST = 'DELETE_POST';
+export const GET_PRIVATE_MESSAGES = 'GET_PRIVATE_MESSAGES';
+export const GET_PRIVATE_MESSAGE = 'GET_PRIVATE_MESSAGE';
+export const ADD_MESSAGE = 'ADD_MESSAGE';
+export const ADD_REPLIES = 'ADD_REPLIES';
+export const DELETE_REPLY = 'DELETE_REPLY';
+export const DELETE_MESSAGE = 'DELETE_MESSAGE';
 
 
 
