178f4fe872736a9246e9c95386830e8da3d11003
Jethro Glaudin <jethro_glaudin@my.uri.edu>
2019-10-23 15:18:58 -0400

    created two component folders for private messaging

---
+++ client/src/components/private-messaging/private-messaging.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react'
+
+class privateMessage extends Component{
+
+    render(){
+        return(
+            <div className="main">
+
+            </div>
+
+        );
+    }
+}
+
+export default privateMessage;
