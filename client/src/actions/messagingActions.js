0ce2e4ff9d228af84267205345b237561e8fbd68
Jethro Glaudin <jethro_glaudin@my.uri.edu>
2019-10-23 15:52:17 -0400

    created messagingActions.js so far just imported the new action types and axios

---
+++ client/src/actions/messagingActions.js
@@ -0,0 +1,9 @@
+import axios from 'axios';
+import {
+    ADD_MESSAGE,
+    GET_ERRORS,
+    GET_PRIVATE_MESSAGE,
+    GET_PRIVATE_MESSAGES,
+    DELETE_MESSAGE,
+    DELETE_REPLY
+} from "./types";
