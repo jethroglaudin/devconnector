d8b0ffdc11d7fe9ab58bfed8fa117cde03d3e553
Jethro Glaudin <jethro_glaudin@my.uri.edu>
2019-10-23 16:15:06 -0400

    updated messageActions with new addMessage action. Making POST http request with axios passing along the messageData. If successful dispatch the response data in a payload as type ADD_MESSAGE

--- client/src/actions/messagingActions.js
+++ client/src/actions/messagingActions.js
@@ -1,9 +1,24 @@
-import axios from 'axios';
+import axios from "axios";
 import {
-    ADD_MESSAGE,
-    GET_ERRORS,
-    GET_PRIVATE_MESSAGE,
-    GET_PRIVATE_MESSAGES,
-    DELETE_MESSAGE,
-    DELETE_REPLY
+  ADD_MESSAGE,
+  GET_ERRORS,
+  GET_PRIVATE_MESSAGE,
+  GET_PRIVATE_MESSAGES,
+  DELETE_MESSAGE,
+  DELETE_REPLY
 } from "./types";
+import { clearErrors } from "./postActions";
+
+// Add Message / Start Conversation
+export const addMessage = messageData => dispatch => {
+  dispatch(clearErrors());
+  axios
+    .post("/api/messaging", messageData)
+    .then(res =>
+      dispatch({
+        type: ADD_MESSAGE,
+        payload: res.data
+      })
+    )
+    .catch(err => dispatch({ GET_ERRORS, payload: err.response.data }));
+};
