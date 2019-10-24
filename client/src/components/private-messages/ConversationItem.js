e9349c1b4169aafc340f8041e539517f1308bfa5
Jethro Glaudin <jethro_glaudin@my.uri.edu>
2019-10-24 11:40:02 -0400

    thus far user can now enter their given  conversations they've created with other users/recepients and they can also leave that conversation also

--- client/src/components/private-messages/ConversationItem.js
+++ client/src/components/private-messages/ConversationItem.js
@@ -1,32 +1,62 @@
-import React, { Component } from 'react';
-import PropTypes from 'prop-types';
-import { connect } from 'react-redux';
+import React, { Component } from "react";
+import PropTypes from "prop-types";
+import { connect } from "react-redux";
+import { Link } from "react-router-dom"
 import { deleteConversation } from "../../actions/messagingActions";
 
 class ConversationItem extends Component {
-    onDeleteClick(id){
-        this.props.deleteConversation(id);
-    }
-    render(){
-        return(
-            <div className="">
-
-            </div>
-        )
-    }
-
+  onDeleteClick(id) {
+    this.props.deleteConversation(id);
+  }
+  render() {
+    const { convos, showActions, auth } = this.props;
+    return (
+      <div className="card card-body mb-3">
+        <div className="row">
+          <div className="col-md-2">
+            <a href="profile.html">
+              <img
+                className="rounded-circle d-none d-md-block"
+                src={auth.user.avatar}
+                alt=""
+              />
+            </a>
+            <br />
+            <p className="text-center">{convos.name}</p>
+          </div>
+       
+        <div className="col-md-10">
+          <p className="lead">{convos.text}</p>
+          {  showActions ? (<span>
+              <Link to={`/messaging/${convos._id}`} className="btn btn-info mr-1">Enter Conversation</Link>
+              {convos.user === auth.user.id ? (
+                  <button
+                  onClick={this.onDeleteClick.bind(this, convos._id)}
+                  type="buddon"
+                  className="btn btn-danger mr-1">Leave Conversation
+                  </button>
+              ): null}
+              </span>): null  }
+        </div>
+        </div>
+      </div>
+    );
+  }
 }
+ConversationItem.defaultProps = {
+  showActions: true
+};
 ConversationItem.propTypes = {
-    deleteConversation: PropTypes.func.isRequired,
-    messaging: PropTypes.object.isRequired,
-    auth: PropTypes.object.isRequired
-}
+  deleteConversation: PropTypes.func.isRequired,
+  messaging: PropTypes.object.isRequired,
+  auth: PropTypes.object.isRequired
+};
 
 const mapStateToProps = state => ({
-    auth: state.auth
-})
+  auth: state.auth
+});
 
 export default connect(
-    mapStateToProps, 
-    { deleteConversation }
-    )(ConversationItem);
+  mapStateToProps,
+  { deleteConversation }
+)(ConversationItem);
