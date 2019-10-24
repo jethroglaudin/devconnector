86a23fab18553a821066db306f751d5a52b21352
Jethro Glaudin <jethro_glaudin@my.uri.edu>
2019-10-24 11:38:41 -0400

    returned a newe Array mapping each conversation and passing the prop to CommentItem

---
+++ client/src/components/private-messages/ConversationFeed.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import CommmentItem from './ConversationItem';
+
+class ConversationFeed extends Component {
+    render(){
+        const { conversations } = this.props;
+        return conversations.map(convos => <CommmentItem key={conversations._id} convos={convos} />)
+    }
+}
+ConversationFeed.propTypes = {
+    conversations: PropTypes.array.isRequired
+}
+
+export default ConversationFeed;
