9e2293fbfc8c4e37ba889b3ad897d025004b5911
Jethro Glaudin <jethro_glaudin@my.uri.edu>
2019-10-23 23:07:29 -0400

    renamed file

---
+++ client/src/components/private-messages/privateMessaging.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react'
+
+class privateMessages extends Component{
+
+    render(){
+        return(
+            <div className="main">
+            <h2>TEST</h2>
+            </div>
+
+        );
+    }
+}
+
+export default privateMessages;
