dd8eaee79667d9be45b14d33e48881e905b2b019
Jethro Glaudin <jethro_glaudin@my.uri.edu>
2019-10-24 01:18:47 -0400

    used PropTypes for messaging and getMessages for validating the data. Also mapped the current state to messaging. Using the lifecyle method to call getMessages

--- client/src/components/private-messages/privateMessages.js
+++ client/src/components/private-messages/privateMessages.js
@@ -1,40 +1,50 @@
-import React, { Component } from 'react';
-import { connect } from 'react-redux';
-import PropTypes from 'prop-types';
-import ConversationItem from '../private-messages/ConversationItem';
-import ConversationFeed from '../private-messages/ConversationFeed';
+import React, { Component } from "react";
+import { connect } from "react-redux";
+import PropTypes from "prop-types";
+import ConversationItem from "../private-messages/ConversationItem";
+import ConversationForm from "../private-messages/ConversationForm";
+import ConversationFeed from "../private-messages/ConversationFeed";
 import Spinner from "../common/Spinner";
 import { getMessages } from "../../actions/messagingActions";
 
-class privateMessages extends Component{
-    componentDidMount(){
-        this.props.getMessages()
-    }
-    render(){
-        const { conversations, loading } = this.props.messaging;
-        let messageContent;
+class privateMessages extends Component {
+  componentDidMount() {
+    this.props.getMessages();
+  }
+  render() {
+    const { conversations, loading } = this.props.messaging;
+    let messageContent;
 
-        if(conversations === null || loading){
-            messageContent = <Spinner />
-        } else {
-            messageContent = <ConversationFeed conversations={conversations} />
-        }
-        return(
-            <div className="main">
-            <h2>TEST</h2>
-            {/* {messageContent} */}
+    if (conversations === null || loading) {
+      messageContent = <Spinner />;
+    } else {
+      messageContent = <ConversationFeed conversations={conversations} />;
+    }
+    return (
+      <div className="feed">
+        <div className="container">
+          <div className="row">
+            <div className="col-md-12">
+              <ConversationForm />
+              {/* {messageContent} */}
+            
             </div>
-
-        );
-    }
+          </div>
+        </div>
+      </div>
+    );
+  }
 }
 privateMessages.propTypes = {
-    getMessages: PropTypes.func.isRequired,
-    messaging: PropTypes.object.isRequired
-}
+  getMessages: PropTypes.func.isRequired,
+  messaging: PropTypes.object.isRequired
+};
+
 const mapStateToProps = state => ({
-    messaging: state.messaging
-})
+  messaging: state.messaging
+});
 
-
-export default connect(mapStateToProps, { getMessages })(privateMessages);
+export default connect(
+  mapStateToProps,
+  { getMessages }
+)(privateMessages);
