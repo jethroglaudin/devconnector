a83a9f9b18aff344f366878487d0c425edf74936
Jethro Glaudin <jethro_glaudin@my.uri.edu>
2019-10-24 10:30:36 -0400

    updated form with recepient input text. setting the value to this.state.recepient.

--- client/src/components/private-messages/ConversationForm.js
+++ client/src/components/private-messages/ConversationForm.js
@@ -1,50 +1,87 @@
-import React, { Component } from 'react';
-import PropTypes from 'prop-types';
-import { connect } from 'react-redux';
-import { addMessage } from '../../actions/messagingActions';
-
+import React, { Component } from "react";
+import PropTypes from "prop-types";
+import { connect } from "react-redux";
+import { addMessage } from "../../actions/messagingActions";
+import TextAreaFieldGroup from "../common/TextAreaFieldGroup";
 
 class ConversationForm extends Component {
-    constructor(props){
-        super(props);
-        this.state = {
-            text: "",
-            errors: {}
-        }
+  constructor(props) {
+    super(props);
+    this.state = {
+      recepient: "",
+      text: "",
+      errors: {}
+    };
 
+    this.onChange = this.onChange.bind(this);
+    this.onSubmit = this.onSubmit.bind(this);
+  }
+
+  componentWillReceiveProps(newProps) {
+    if (newProps.errors) {
+      this.setState({ errors: newProps.errors });
     }
+  }
 
-    componentWillReceiveProps(newProps){
-        if(newProps.errors) {
-            this.setState({errors: newProps.errors})
-        }
-    }
+  onSubmit(e) {
+    e.preventDefault();
+    const { user } = this.props.auth;
+
+    const newMessage = {
+      user: user.id,
+      recepient: this.state.recepient,
+      text: this.state.text,
+      name: user.name,
+      avatar: user.avatar
+    };
+    this.props.addMessage(newMessage);
+    this.setState({ text: "", recepient: "" });
+  }
+  onChange(e) {
+    this.setState({ [e.target.name]: e.target.value });
+  }
 
 
+  render() {
+    const { errors } = this.state;
 
-    
+    return (
+      <div className="post-form mb-3">
+        {/* <div className="card card-info">
+          <div className="card-header bg-info text-white">Say Somthing...</div>
+        </div> */}
+       Recepient: <input type="text" name="recepient" value={this.state.recepient} onChange={this.onChange} />
+        <div className="card-body">
+          <form onSubmit={this.onSubmit}>
+              <div className="form-group">
+                  <TextAreaFieldGroup
+                  placeholder="Enter Message"
+                  name="text"
+                  value={this.state.text}
+                  onChange={this.onChange}
+                  error={errors.text}
+                  />
 
-    render(){
-        const { user } = this.props.auth;
-        return(
-            <div className="main">
-
-            </div>
-        )
-    }
-
+              </div>
+              <button type="submit" className="btn btn-dark">Submit</button>
+          </form>
+        </div>
+      </div>
+    );
+  }
 }
 ConversationForm.propTypes = {
-    addMessage: PropTypes.func.isRequired,
-    auth: PropTypes.object.isRequired,
-    errors: PropTypes.object.isRequired
-}
+  addMessage: PropTypes.func.isRequired,
+  auth: PropTypes.object.isRequired,
+  errors: PropTypes.object.isRequired
+};
 
 const mapStateToProps = state => ({
-    auth: state.auth,
-    errors: state.errors
-})
+  auth: state.auth,
+  errors: state.errors
+});
 
-
-
-export default connect(mapStateToProps, { addMessage })(ConversationForm);
+export default connect(
+  mapStateToProps,
+  { addMessage }
+)(ConversationForm);
