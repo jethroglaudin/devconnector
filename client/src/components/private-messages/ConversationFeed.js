622cdb933f709beea6754b9407c38b30ff329d85
Jethro Glaudin <jethro_glaudin@my.uri.edu>
2019-10-25 21:30:30 -0400

    updated component

--- client/src/components/private-messages/ConversationFeed.js
+++ client/src/components/private-messages/ConversationFeed.js
@@ -5,7 +5,7 @@ import CommmentItem from './ConversationItem';
 class ConversationFeed extends Component {
     render(){
         const { conversations } = this.props;
-        return conversations.map(convos => <CommmentItem key={conversations._id} convos={convos} />)
+        return conversations.map(convos => <CommmentItem key={convos._id} convos={convos} />)
     }
 }
 ConversationFeed.propTypes = {
