11cb71e829c3cc476734c08458c11453c7b6a0cc
Jethro Glaudin <jethro_glaudin@my.uri.edu>
2019-10-25 21:32:51 -0400

    mapped through replies from props passed the reply and id to MessagingItem

--- client/src/components/private-messaging/MessagingFeed.js
+++ client/src/components/private-messaging/MessagingFeed.js
@@ -1,15 +1,17 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import MessagingItem from './MessagingItem';
 
 class MessagingFeed extends Component{
 
     render(){
-        return(
-            <div className="main">
-
-            </div>
-
-        );
+        const { replies, replyId } = this.props
+        return replies.map(reply => (<MessagingItem key={reply._id} reply={reply} replyId={replyId} />))
     }
+}
+MessagingFeed.propTypes = {
+    replies: PropTypes.array.isRequired,
+    replyId: PropTypes.string.isRequired
 }
 
 export default MessagingFeed;
