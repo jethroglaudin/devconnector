9420eb194510e3c8012c0675cc5a6545fd150cc7
Jethro Glaudin <jethro_glaudin@my.uri.edu>
2019-10-30 23:46:59 -0400

    changed getMessage payload for GET_ERRORS action type to null

--- client/src/actions/messagingActions.js
+++ client/src/actions/messagingActions.js
@@ -63,7 +63,7 @@ export const getMessage = id => dispatch => {
     .catch(err =>
       dispatch({
         type: GET_ERRORS,
-        payload: err.response.data
+        payload: null
       })
     );
 };
